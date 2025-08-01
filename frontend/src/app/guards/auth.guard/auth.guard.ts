c7a571f1ff1907cf1e5bc9d432b2c0e7cc54e48b
cai360 <archery0360@gmail.com>
2025-08-01 22:38:18 +0800

    (ui)login fix

---
+++ frontend/src/app/guards/auth.guard/auth.guard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth.guard',
+  imports: [],
+  templateUrl: './auth.guard.html',
+  styleUrl: './auth.guard.scss'
+})
+export class AuthGuard {
+
+}
