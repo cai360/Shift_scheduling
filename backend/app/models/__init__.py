4d0486d47a329a9d482fc20d2ef090329f495326
cai360 <archery0360@gmail.com>
2025-11-29 20:51:33 +0800

    feat(models/users): add users model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.extensions import db
+from .base import BaseModel
 from .user import User
 from .shift import Shift
 from .shift_assignments import ShiftAssignment
@@ -7,4 +8,3 @@ from .availability import Availability
 from .companies import Company
 from .companies_users import CompanyUser
 from .schedule import Schedule
-from .base import BaseModel
