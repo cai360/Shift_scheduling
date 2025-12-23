53034891433beb13c7c242b526575c8b5b5c057f
Ansel Wu <anselwu@fucotech.com.tw>
2025-12-23 16:08:16 +0800

    fix: Update availability table schema & integrate migrations

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,7 +3,7 @@ from .base import BaseModel
 from .user import User
 from .shift import Shift
 from .shift_assignments import ShiftAssignment
-from .availability import Availability
+from .unavailability import Unavailability
 from .companies import Company
 from .companies_users import CompanyUser
 from .shift_takeovers import ShiftTakeover
--- backend/app/models/unavailability.py
+++ backend/app/models/unavailability.py
@@ -3,8 +3,8 @@ from sqlalchemy.dialects.postgresql import UUID
 from .base import BaseModel
 
 
-class Availability(BaseModel):
-    __tablename__ = 'availabilities'
+class Unavailability(BaseModel):
+    __tablename__ = 'unavailabilities'
 
     user_id = db.Column(
         UUID(as_uuid=True),
@@ -18,18 +18,16 @@ class Availability(BaseModel):
         nullable=False
     )
 
-    date = db.Column(db.Date, nullable=False)
-    starting_time = db.Column(db.Time, nullable=False)
-    ending_time = db.Column(db.Time, nullable=False)
+    starting_time = db.Column(db.DateTime, nullable=False)
+    ending_time = db.Column(db.DateTime, nullable=False)
 
     __table_args__ = (
         db.UniqueConstraint(
             'company_id', 
             'user_id', 
-            'date', 
             'starting_time', 
             'ending_time',
-            name='uq_company_user_availability'
+            name='uq_company_user_unavailability'
         ),
     )
 
