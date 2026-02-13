ec28d0c1352bde92972aeec725810e1870c51370
Ansel Wu <anselwu@fucotech.com.tw>
2026-02-13 14:06:56 +0800

    test

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,31 +1,35 @@
-import AppButton from "../components/ui/AppButton"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import AppButton from '../components/ui/AppButton';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
-    const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const a = 1;
+  console.log(a);
 
-    const handleLogout = () => {
-        try {
-            setLoading(true)
-            localStorage.removeItem('token')
-            localStorage.removeItem('refresh_token')
-            navigate('/')
-            console.log('Logout Success')
-        } catch (err) {
-            console.error('Logout Failed', err)
-        } finally {
-            setLoading(false)
-        }
+  const handleLogout = () => {
+    try {
+      setLoading(true);
+      localStorage.removeItem('token');
+      localStorage.removeItem('refresh_token');
+      navigate('/');
+      console.log('Logout Success');
+    } catch (err) {
+      console.error('Logout Failed', err);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <AppButton loading={loading} onClick={handleLogout}>登出</AppButton>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <AppButton loading={loading} onClick={handleLogout}>
+        登出
+      </AppButton>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
