f98c50f9f3487fbff6ef3d4e13821505c01186af
cai360 <archery0360@gmail.com>
2026-03-07 15:28:48 +0800

    refector(delete company): move permission check into controlled level to prevent circute dependency

--- backend/app/services/companyUser_service.py
+++ backend/app/services/companyUser_service.py
@@ -3,6 +3,8 @@ from app.models.user import User
 from app.extensions import db
 from app.models.companies_users import CompanyUser 
 from sqlalchemy.orm import selectinload
+from app.services.company_service import CompanyService
+
 
 class CompanyUserService:
     @staticmethod
@@ -28,3 +30,45 @@ class CompanyUserService:
             CompanyUser.company_id == company_id,
             CompanyUser.deleted_at.is_(None)
         ).all()
+    
+    @staticmethod
+    def join_company(company_id, user_id):
+        """MVP: Only creator is manager. Everyone else is member."""
+        CompanyService.get_company(company_id)
+
+        # Is active membership or not
+        existing = CompanyUser.query.filter(
+            CompanyUser.company_id == company_id,
+            CompanyUser.user_id == user_id,
+            CompanyUser.deleted_at.is_(None)
+        ).first()
+
+        if existing:
+            return existing
+
+        # Previously deleted -> restore
+        soft_deleted = CompanyUser.query.filter(
+            CompanyUser.company_id == company_id,
+            CompanyUser.user_id == user_id,
+            CompanyUser.deleted_at.is_not(None)
+        ).first()
+
+        if soft_deleted:
+            soft_deleted.deleted_at = None
+            soft_deleted.role = "employee"
+            db.session.commit()
+            return soft_deleted
+
+        # First time join
+        company_user = CompanyUser(
+            company_id=company_id,
+            user_id=user_id,
+            role="employee"
+        )
+        db.session.add(company_user)
+        db.session.commit()
+        return company_user
+
+    
+
+        
--- backend/app/services/company_service.py
+++ backend/app/services/company_service.py
@@ -2,7 +2,6 @@ from app.models.companies import Company
 from app.extensions import db
 from app.models.companies_users import CompanyUser
 from datetime import datetime, timezone
-from app.services.permission_services import PermissionService
 
 class CompanyService:
 
@@ -68,56 +67,11 @@ class CompanyService:
         '''
         company = CompanyService.get_company(company_id)
 
-        PermissionService.require_owner(
-            company_id=company_id, 
-            user_id=user_id
-        )
-
         company.deleted_at = datetime.now(timezone.utc)
         db.session.commit()
         return True
 
 
-    @staticmethod
-    def join_company(company_id, user_id):
-        """MVP: Only creator is manager. Everyone else is member."""
-        CompanyService.get_company(company_id)
-
-        # Is active membership or not
-        existing = CompanyUser.query.filter(
-            CompanyUser.company_id == company_id,
-            CompanyUser.user_id == user_id,
-            CompanyUser.deleted_at.is_(None)
-        ).first()
-
-        if existing:
-            return existing
-
-        # Previously deleted -> restore
-        soft_deleted = CompanyUser.query.filter(
-            CompanyUser.company_id == company_id,
-            CompanyUser.user_id == user_id,
-            CompanyUser.deleted_at.is_not(None)
-        ).first()
-
-        if soft_deleted:
-            soft_deleted.deleted_at = None
-            soft_deleted.role = "employee"
-            db.session.commit()
-            return soft_deleted
-
-        # First time join
-        company_user = CompanyUser(
-            company_id=company_id,
-            user_id=user_id,
-            role="employee"
-        )
-        db.session.add(company_user)
-        db.session.commit()
-        return company_user
-
-    
-
-        
+  
         
 
