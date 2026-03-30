3e43615500ee539da5bee8cf2b269b38afbec535
cai360 <archery0360@gmail.com>
2026-03-30 11:09:17 +0800

    feat(assignment_service): add validation with unavaliability

--- backend/app/services/assignment_service.py
+++ backend/app/services/assignment_service.py
@@ -1,10 +1,10 @@
 from app.extensions import db
-from app.models import ShiftAssignment, Shift
+from app.models import ShiftAssignment, Shift, Unavailability
 from app.services.company_service import CompanyService
 from app.services.companyUser_service import CompanyUserService
 from app.services.permission_services import PermissionService
 from app.errors.assignment import AssignmentConflictError, AssignmentCapacityExceededError
-from datetime import datetime, timezone
+from datetime import datetime
 from app.config import UTC_TZ
 from sqlalchemy.orm import selectinload
 from sqlalchemy import func
@@ -20,51 +20,36 @@ class AssignmentService:
         if not user:
             raise ValueError("member doesn't exist")
         
+        unique_shift_ids = list(dict.fromkeys(shift_ids))
+        if len(unique_shift_ids) != len(shift_ids):
+            raise ValueError("shift_ids must be unique")
+        
         shifts = (
             Shift.query
             .filter(
                 Shift.company_id == company_id,
-                Shift.id.in_(shift_ids),
+                Shift.id.in_(unique_shift_ids),
                 Shift.deleted_at.is_(None),
                 Shift.published_at.isnot(None)
             ).all()
         )
 
-        if len(shifts) != len(set(shift_ids)):
+        if len(shifts) != len(unique_shift_ids):
             raise ValueError("Some shifts are invalid, not published, or not in this company")
         
-        conflicts = (
-            ShiftAssignment.query.filter(
-                ShiftAssignment.user_id == target_user_id,
-                ShiftAssignment.shift_id.in_(shift_ids),
-                ShiftAssignment.deleted_at.is_(None)
-            ).all()
+        AssignmentService._validate_already_assigned(
+            target_user_id=target_user_id,
+            shift_ids = shift_ids
         )
 
-        if conflicts:
-            conflict_shift_ids = [a.shift_id for a in conflicts]
-            raise AssignmentConflictError(conflict_shift_ids=conflict_shift_ids)
+        AssignmentService._validate_capacity(shifts = shifts)
         
-        count = (
-            db.session.query(
-                ShiftAssignment.shift_id,
-                func.count(ShiftAssignment.id).label("count")
-            ).filter(
-                ShiftAssignment.shift_id.in_([s.id for s in shifts]),
-                ShiftAssignment.deleted_at.is_(None)
-            ).group_by(ShiftAssignment.shift_id)
-            .all()
+        AssignmentService._validate_with_unavailability(
+            company_id=company_id,
+            target_user_id=target_user_id,
+            shifts=shifts
         )
-    
-        count_map = {shift_id: count for shift_id, count in count}
-        over_capacity_shift_ids = []
-        for shift in shifts:
-            if count_map.get(shift.id, 0) >= shift.capacity:
-                over_capacity_shift_ids.append(shift.id)
 
-        if over_capacity_shift_ids:
-            raise AssignmentCapacityExceededError(over_capacity_shift_ids)
-            
         for shift in shifts:
             assignment = ShiftAssignment(
                 user_id=target_user_id,
@@ -111,6 +96,68 @@ class AssignmentService:
             assignment.deleted_at = now
 
         db.session.commit()
+
+
+    @staticmethod
+    def _validate_with_unavailability(*, company_id, target_user_id, shifts):
+        conflict_shift_ids = []
+
+        for shift in shifts:
+            has_conflict = (
+                Unavailability.query.filter(
+                    Unavailability.company_id == company_id,
+                    Unavailability.user_id == target_user_id,
+                    Unavailability.deleted_at.is_(None),
+                    Unavailability.start_at < shift.end_at,
+                    Unavailability.end_at > shift.start_at,
+                )
+                .first()
+                is not None
+            )
+
+            if has_conflict:
+                conflict_shift_ids.append(shift.id)
+
+        if conflict_shift_ids:
+            raise AssignmentConflictError(conflict_shift_ids=conflict_shift_ids)
+        
+
+    @staticmethod
+    def _validate_already_assigned(*, target_user_id, shift_ids):
+        conflicts = (
+            ShiftAssignment.query.filter(
+                ShiftAssignment.user_id == target_user_id,
+                ShiftAssignment.shift_id.in_(shift_ids),
+                ShiftAssignment.deleted_at.is_(None)
+            ).all()
+        )
+
+        if conflicts:
+            conflict_shift_ids = [a.shift_id for a in conflicts]
+            raise AssignmentConflictError(conflict_shift_ids=conflict_shift_ids)
+        
+    @staticmethod
+    def _validate_capacity(shifts):
+        count = (
+            db.session.query(
+                ShiftAssignment.shift_id,
+                func.count(ShiftAssignment.id).label("count")
+            ).filter(
+                ShiftAssignment.shift_id.in_([s.id for s in shifts]),
+                ShiftAssignment.deleted_at.is_(None)
+            ).group_by(ShiftAssignment.shift_id)
+            .all()
+        )
+        count_map = {shift_id: count for shift_id, count in count}
+
+        over_capacity_shift_ids = []
+        for shift in shifts:
+            if count_map.get(shift.id, 0) >= shift.capacity:
+                over_capacity_shift_ids.append(shift.id)
+
+        if over_capacity_shift_ids:
+            raise AssignmentCapacityExceededError(over_capacity_shift_ids)
+
 
 
 
