a172582e940c71e93fee3cc918d45b3079fe4562
Ansel Wu <anselwu@fucotech.com.tw>
2026-05-15 12:17:40 +0800

    chore(db): merge alembic heads

---
+++ backend/migrations/versions/54d1c5b3895d_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 54d1c5b3895d
+Revises: 1502877794e3, ae1f63339628
+Create Date: 2026-05-15 12:13:40.395423
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '54d1c5b3895d'
+down_revision = ('1502877794e3', 'ae1f63339628')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
