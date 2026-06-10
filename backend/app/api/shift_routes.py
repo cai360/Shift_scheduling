01dafb79dbe5bdb00294238e430cad5aefb74a4f
cai360 <archery0360@gmail.com>
2026-06-10 15:54:39 +0800

    fix(shift) query key word bug

--- backend/app/api/shift_routes.py
+++ backend/app/api/shift_routes.py
@@ -34,8 +34,8 @@ def get_shifts(company_id):
     """
     query = ShiftQuerySchema().load(request.args)
     try:
-        from_ = parse_datetime(query["from"]) if "from" in query else None
-        to_ = parse_datetime(query["to"]) if "to" in query else None
+        from_ = parse_datetime(query["from_"]) if query.get("from_") else None
+        to_ = parse_datetime(query["to_"]) if query.get("to_") else None
     except ValueError:
         raise ValueError("Invalid datetime format.")
 
