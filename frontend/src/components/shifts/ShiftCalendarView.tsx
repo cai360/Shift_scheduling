806eebd240b70db90ec35d223f0980733ecd6be2
cai360 <archery0360@gmail.com>
2026-06-22 15:04:50 +0800

    feat(shift-view): adjust time range in shift dynamically

--- frontend/src/components/shifts/ShiftCalendarView.tsx
+++ frontend/src/components/shifts/ShiftCalendarView.tsx
@@ -6,9 +6,10 @@ import type { EventInput } from '@fullcalendar/core';
 
 type Props = {
   shifts: Shift[];
+  onRangeChange: (from: string, to: string) => void;
 };
 
-const ShiftCalendarView = ({ shifts }: Props) => {
+const ShiftCalendarView = ({ shifts, onRangeChange }: Props) => {
   const events: EventInput[] = useMemo(() => {
     return shifts.map((shift) => ({
       id: shift.id,
@@ -18,12 +19,61 @@ const ShiftCalendarView = ({ shifts }: Props) => {
     }));
   }, [shifts]);
 
+  const calendarRange = useMemo(() => {
+    if (shifts.length === 0) {
+      return {
+        slotMinTime: '08:00:00',
+        slotMaxTime: '18:00:00',
+      };
+    }
+    const hasOvernightShift = shifts.some((shift) => {
+      const start = new Date(shift.start_at);
+      const end = new Date(shift.end_at);
+      return start.toDateString() !== end.toDateString();
+    });
+
+    if (hasOvernightShift) {
+      return {
+        slotMinTime: '00:00:00',
+        slotMaxTime: '24:00:00',
+      };
+    }
+
+    const earliest = Math.min(
+      ...shifts.map((shift) => new Date(shift.start_at).getTime()),
+    );
+
+    const latest = Math.max(
+      ...shifts.map((shift) => new Date(shift.end_at).getTime()),
+    );
+
+    const earliestDate = new Date(earliest);
+    const latestDate = new Date(latest);
+
+    earliestDate.setHours(Math.max(0, earliestDate.getHours() - 1));
+    latestDate.setHours(Math.min(23, latestDate.getHours() + 1));
+
+    return {
+      slotMinTime: `${earliestDate
+        .getHours()
+        .toString()
+        .padStart(2, '0')}:00:00`,
+
+      slotMaxTime: `${latestDate.getHours().toString().padStart(2, '0')}:00:00`,
+    };
+  }, [shifts]);
+
   return (
     <FullCalendar
       plugins={[timeGridPlugin]}
       initialView="timeGridWeek"
       weekends={true}
       events={events}
+      slotMinTime={calendarRange.slotMinTime}
+      slotMaxTime={calendarRange.slotMaxTime}
+      datesSet={(arg) => {
+        onRangeChange(arg.start.toISOString(), arg.end.toISOString());
+      }}
     />
   );
 };
--- frontend/src/pages/ShiftManagementPage.tsx
+++ frontend/src/pages/ShiftManagementPage.tsx
@@ -1,39 +1,50 @@
 import ShiftCalendarView from '../components/shifts/ShiftCalendarView';
 import { useCompanyContext } from '../contexts/useCompanyContext';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getShifts, type Shift } from '../services/shift.api';
 
-const getCurrentMonthRange = () => {
-  const now = new Date();
-
-  const from = new Date(now.getFullYear(), now.getMonth(), 1);
-  const to = new Date(now.getFullYear(), now.getMonth() + 1, 0);
-
-  return {
-    from: from.toISOString().slice(0, 10),
-    to: to.toISOString().slice(0, 10),
-  };
+type DateRange = {
+  from: string;
+  to: string;
 };
+
+// const getCurrentMonthRange = () => {
+//   const now = new Date();
+
+//   const from = new Date(now.getFullYear(), now.getMonth(), 1);
+//   const to = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+
+//   return {
+//     from: from.toISOString().slice(0, 10),
+//     to: to.toISOString().slice(0, 10),
+//   };
+// };
 
 const ShiftManagementPage = () => {
   const { currentCompanyId } = useCompanyContext();
   const [shifts, setShifts] = useState<Shift[]>([]);
-
-  const monthRange = useMemo(() => getCurrentMonthRange(), []);
+  const [range, setRange] = useState<DateRange | null>(null);
 
   useEffect(() => {
-    if (!currentCompanyId) return;
+    if (!currentCompanyId || !range) return;
 
     getShifts(currentCompanyId, {
-      from: monthRange.from,
-      to: monthRange.to,
+      from: range.from,
+      to: range.to,
     }).then((data) => {
       console.log('shifts:', data);
       setShifts(data);
     });
-  }, [currentCompanyId, monthRange]);
+  }, [currentCompanyId, range]);
 
-  return <ShiftCalendarView shifts={shifts} />;
+  return (
+    <ShiftCalendarView
+      shifts={shifts}
+      onRangeChange={(from, to) => {
+        setRange({ from, to });
+      }}
+    />
+  );
 };
 
 export default ShiftManagementPage;
